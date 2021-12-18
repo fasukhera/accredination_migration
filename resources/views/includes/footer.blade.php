406cef216044572f732e83ef13c0fab2c5addb00
fasukhera <fasukhera@gmail.com>
2021-12-19 01:55:31 +0500

    landing page

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,86 @@
+<!--=====================================-->
+<!--=     Footer Section Area Start     =-->
+<!--=====================================-->
+<footer id="footer-wrap-layout1" class="footer-wrap-layout1 bg-color-light">
+    <div class="footer-top-layout1 bg-no-repeat bg-size-cover bg-position-center"
+         data-bg-image="media/element/element3.png">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-6 col-12">
+                    <div class="footer-widgets">
+                        <div class="footer-logo">
+                            <a href="index.html"><img src="{{asset('logo-black.png')}}" alt="Logo"></a>
+                        </div>
+                        <p class="about-paragraph">Building Quality and Integrity One Nation at a Time</p>
+                        <ul class="footer-social">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="d-none col-xl-3 d-xl-block" style="display: none !important;">
+                    <div class="footer-widgets">
+                        <h3 class="footer-widget-heading">Certifications</h3>
+                        {{--                                <ul class="footer-menu">--}}
+                        {{--                                    <li><a href="#">Management</a></li>--}}
+                        {{--                                    <li><a href="#">Reporting</a></li>--}}
+                        {{--                                    <li><a href="#">Support</a></li>--}}
+                        {{--                                    <li><a href="#">Community</a></li>--}}
+                        {{--                                    <li><a href="#">Customers</a></li>--}}
+                        {{--                                </ul>--}}
+                    </div>
+                </div>
+                <div class="d-none col-xl-3 col-lg-4 col-md-7 col-12">
+                    <div class="footer-widgets">
+                        <h3 class="footer-widget-heading">Higher Education Qualifications</h3>
+                        {{--                                <ul class="footer-menu">--}}
+                        {{--                                    <li><a href="about.html">About Us</a></li>--}}
+                        {{--                                    <li><a href="#">How to work</a></li>--}}
+                        {{--                                    <li><a href="contact.html">Contact Us</a></li>--}}
+                        {{--                                    <li><a href="#">Our Media</a></li>--}}
+                        {{--                                    <li><a href="#">Privacy</a></li>--}}
+                        {{--                                </ul>--}}
+                    </div>
+                </div>
+                <div class="d-none col-xl-3 col-lg-4 col-md-7 col-12">
+                    <div class="footer-widgets">
+                        <h3 class="footer-widget-heading">Consulting</h3>
+                        {{--                                <ul class="footer-menu">--}}
+                        {{--                                    <li><a href="service-details.html">Web Design</a></li>--}}
+                        {{--                                    <li><a href="service-details.html">Development</a></li>--}}
+                        {{--                                    <li><a href="service-details.html">Graphic Design</a></li>--}}
+                        {{--                                    <li><a href="service-details.html">Branding</a></li>--}}
+                        {{--                                    <li><a href="service-details.html">Creative Solution</a></li>--}}
+                        {{--                                </ul>--}}
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-6"></div>
+                <div class="col-xl-3 col-lg-4 col-md-7 col-12">
+                    <div class="footer-widgets">
+                        <h3 class="footer-widget-heading">Contact Info</h3>
+                        <p>Grursus mal suada faci lisis Lorem to ipsum consectetur.</p>
+                        <ul class="footer-contact">
+                            <li><i class="flaticon-placeholder"></i>113 Sassnex, White House, New Jercy, USA
+                            </li>
+                            <li><i class="flaticon-plane"></i>info@yourdomain.com</li>
+                            <li><i class="flaticon-telephone"></i>+001 548 159 2491</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom-layout1 bg-color-accent">
+        <div class="container">
+            <div class="copy-right-wrap">
+                <p class="copy-right-text">© {{Date('Y')}} Accredination. All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--=====================================-->
+<!--=      Footer Section Area End      =-->
+<!--=====================================-->
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,34 @@
+<head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Accredination</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="media/favicon.png">
+    <!-- Dependency Styles -->
+    <link rel="stylesheet" href="{{asset('dependencies/bootstrap/css/bootstrap.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/fontawesome/css/all.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/select2/css/select2.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/flaticon/flaticon.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/wow/css/animate.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/page-piling/css/jquery.pagepiling.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/nivo-slider/css/nivo-slider.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/meanmenu/css/meanmenu.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/magnific-popup/css/magnific-popup.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/owl.carousel/css/owl.carousel.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/owl.carousel/css/owl.theme.default.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('dependencies/slick/css/slick.css" type="text/css')}}">
+    <link rel="stylesheet" href="{{asset('dependencies/slick/css/slick-theme.css" type="text/css')}}">
+
+    <!-- Site Stylesheet -->
+    <link rel="stylesheet" href="assets/css/app.css" type="text/css">
+    <!-- Animation Stylesheet -->
+    <link rel="stylesheet" href="{{asset('dependencies/animation-css/animation-css.css')}}" type="text/css">
+
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+
+</head>
