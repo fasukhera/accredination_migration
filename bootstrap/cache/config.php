9d83badbe5d32dfba9e8a700e71c557b713adcf9
Ahmed Yusuf Mohamed <61713723+Ahmed7752@users.noreply.github.com>
2022-01-07 13:49:50 +0000

    updated .gitignore file
    added bootstrap config file to gitignore

--- bootstrap/cache/config.php
+++ bootstrap/cache/config.php
@@ -1,7 +1,7 @@
 <?php return array (
   'app' => 
   array (
-    'name' => 'Accredination',
+    'name' => 'Laravel',
     'env' => 'local',
     'debug' => true,
     'url' => 'http://localhost',
@@ -10,7 +10,7 @@
     'locale' => 'en',
     'fallback_locale' => 'en',
     'faker_locale' => 'en_US',
-    'key' => 'base64:sGLrzOrkyXZz2avXgLKLpmj98ZgRQW81AwMj32A3uZg=',
+    'key' => 'base64:kI/7XzltPihHTH17ma7cied12hdaivGLyk/34jX+BI0=',
     'cipher' => 'AES-256-CBC',
     'providers' => 
     array (
@@ -177,7 +177,7 @@
       'file' => 
       array (
         'driver' => 'file',
-        'path' => 'C:\\xampp\\htdocs\\accredination\\storage\\framework/cache/data',
+        'path' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\framework/cache/data',
       ),
       'memcached' => 
       array (
@@ -216,7 +216,7 @@
         'endpoint' => NULL,
       ),
     ),
-    'prefix' => 'accredination_cache',
+    'prefix' => 'laravel_cache',
   ),
   'cors' => 
   array (
@@ -254,7 +254,7 @@
       array (
         'driver' => 'sqlite',
         'url' => NULL,
-        'database' => 'accredination',
+        'database' => 'laravel',
         'prefix' => '',
         'foreign_key_constraints' => true,
       ),
@@ -264,7 +264,7 @@
         'url' => NULL,
         'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'accredination',
+        'database' => 'laravel',
         'username' => 'root',
         'password' => '',
         'unix_socket' => '',
@@ -284,7 +284,7 @@
         'url' => NULL,
         'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'accredination',
+        'database' => 'laravel',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',
@@ -299,7 +299,7 @@
         'url' => NULL,
         'host' => '127.0.0.1',
         'port' => '3306',
-        'database' => 'accredination',
+        'database' => 'laravel',
         'username' => 'root',
         'password' => '',
         'charset' => 'utf8',
@@ -314,7 +314,7 @@
       'options' => 
       array (
         'cluster' => 'redis',
-        'prefix' => 'accredination_database_',
+        'prefix' => 'laravel_database_',
       ),
       'default' => 
       array (
@@ -343,12 +343,12 @@
       'local' => 
       array (
         'driver' => 'local',
-        'root' => 'C:\\xampp\\htdocs\\accredination\\storage\\app',
+        'root' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\app',
       ),
       'public' => 
       array (
         'driver' => 'local',
-        'root' => 'C:\\xampp\\htdocs\\accredination\\storage\\app/public',
+        'root' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\app/public',
         'url' => 'http://localhost/storage',
         'visibility' => 'public',
       ),
@@ -365,7 +365,7 @@
     ),
     'links' => 
     array (
-      'C:\\xampp\\htdocs\\accredination\\public\\storage' => 'C:\\xampp\\htdocs\\accredination\\storage\\app/public',
+      'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\public\\storage' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\app/public',
     ),
   ),
   'hashing' => 
@@ -399,13 +399,13 @@
       'single' => 
       array (
         'driver' => 'single',
-        'path' => 'C:\\xampp\\htdocs\\accredination\\storage\\logs/laravel.log',
+        'path' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\logs/laravel.log',
         'level' => 'debug',
       ),
       'daily' => 
       array (
         'driver' => 'daily',
-        'path' => 'C:\\xampp\\htdocs\\accredination\\storage\\logs/laravel.log',
+        'path' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\logs/laravel.log',
         'level' => 'debug',
         'days' => 14,
       ),
@@ -455,7 +455,7 @@
       ),
       'emergency' => 
       array (
-        'path' => 'C:\\xampp\\htdocs\\accredination\\storage\\logs/laravel.log',
+        'path' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\logs/laravel.log',
       ),
     ),
   ),
@@ -467,9 +467,9 @@
       'smtp' => 
       array (
         'transport' => 'smtp',
-        'host' => 'smtp.gmail.com',
-        'port' => '587',
-        'encryption' => 'tls',
+        'host' => 'smtp.mailtrap.io',
+        'port' => '2525',
+        'encryption' => NULL,
         'username' => NULL,
         'password' => NULL,
         'timeout' => NULL,
@@ -504,15 +504,15 @@
     ),
     'from' => 
     array (
-      'address' => 'hello@example.com',
-      'name' => 'Example',
+      'address' => NULL,
+      'name' => 'Laravel',
     ),
     'markdown' => 
     array (
       'theme' => 'default',
       'paths' => 
       array (
-        0 => 'C:\\xampp\\htdocs\\accredination\\resources\\views/vendor/mail',
+        0 => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\resources\\views/vendor/mail',
       ),
     ),
   ),
@@ -591,7 +591,7 @@
     'lifetime' => '120',
     'expire_on_close' => false,
     'encrypt' => false,
-    'files' => 'C:\\xampp\\htdocs\\accredination\\storage\\framework/sessions',
+    'files' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\framework/sessions',
     'connection' => NULL,
     'table' => 'sessions',
     'store' => NULL,
@@ -600,7 +600,7 @@
       0 => 2,
       1 => 100,
     ),
-    'cookie' => 'accredination_session',
+    'cookie' => 'laravel_session',
     'path' => '/',
     'domain' => NULL,
     'secure' => NULL,
@@ -611,9 +611,9 @@
   array (
     'paths' => 
     array (
-      0 => 'C:\\xampp\\htdocs\\accredination\\resources\\views',
+      0 => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\resources\\views',
     ),
-    'compiled' => 'C:\\xampp\\htdocs\\accredination\\storage\\framework\\views',
+    'compiled' => 'C:\\Users\\Mayfair1\\Documents\\WORK\\Accredination-master\\storage\\framework\\views',
   ),
   'flare' => 
   array (
